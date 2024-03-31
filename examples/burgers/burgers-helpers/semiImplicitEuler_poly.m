892908e9536de17b7a2361d33ba1f0f4578f4629
Jin Yi Yong <jinyi12@outlook.com>
2024-03-31 16:56:35 -0400

    Added fix for time integrator that includes polynomial and constant terms

--- examples/burgers/burgers-helpers/semiImplicitEuler_poly.m
+++ examples/burgers/burgers-helpers/semiImplicitEuler_poly.m
@@ -1,31 +1,43 @@
 %% semi-implicit Euler scheme for integrating learned model from zero initial condition
-function s_hat = semiImplicitEuler_poly(Ahat, Fhat, Bhat, Phat, dt, u_input, IC)
+function s_hat = semiImplicitEuler_poly(Chat, Ahat, Fhat, Bhat, Phat, dt, u_input, IC)
     K = size(u_input,1);
     r = size(Ahat,1);
     s_hat = zeros(r,K+1); % initial state is zeros everywhere
     
     s_hat(:,1) = IC;
 
+    % disp('Size of Ahat')
+    % disp(size(Ahat))
+    % disp('Size pf Fhat')
+    % disp(size(Fhat))
+    % disp('Size pf Phat')
+    % disp(size(Phat))
+
+    % disp(Ahat)
+    % disp(Fhat)
+    % disp(Phat)
+
     ImdtA = eye(r) - dt*Ahat;
     for i = 1:K
         ssq = get_x_sq(s_hat(:,i)')';
-        gs = gen_poly(s_hat(:, i)', 2)';
-        disp("ImdtA");
-        disp(ImdtA);
-        disp("shat");
-        disp(size(s_hat(:, i)));
+        gs = gen_poly(s_hat(:, i)', 0)';
 
-        disp("dtFhat");
-        disp(size(dt*Fhat*ssq))
+        if isempty(Bhat)
+            s_hat(:,i+1) = ImdtA\(s_hat(:,i) + dt*Fhat*ssq + dt*Phat*gs + dt*Chat);
+            % disp((s_hat(:,i) + dt*Fhat*ssq + dt*Phat*gs))
+            % disp(dt * Fhat * ssq)
+            
+        else
+            % disp(size(Bhat))
+            % disp(size(u_input(i)))
+            % disp(size(Fhat*ssq))
+            % disp(size(Phat))
+            % disp(size(gs))
+            Bhat_term = dt*Bhat*u_input(i);
+            s_hat(:,i+1) = ImdtA\(s_hat(:,i) + dt*Fhat*ssq + Bhat_term + dt*Phat*gs + dt*Chat);
+        end        
 
-        disp("dtBhat")
-        disp(size(dt*Bhat*u_input(i)))
-
-        disp("dtPhat")
-        disp(size(dt*Phat*gs));
-        % disp((s_hat(:,i) + dt*Fhat*ssq + dt*Bhat*u_input(i) + dt*Phat*gs));
-
-        s_hat(:,i+1) = ImdtA\(s_hat(:,i) + dt*Fhat*ssq + dt*Bhat*u_input(i) + dt*Phat*gs);
+        
         if any(isnan(s_hat(:,i+1)))
             warning(['ROM unstable at ',num2str(i),'th timestep'])
             break
